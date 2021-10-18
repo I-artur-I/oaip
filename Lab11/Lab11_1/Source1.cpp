8e7b06e58e5f38d27cc3fa09a82286b7de5791b5
VloBoo <VloBo2004@gmail/com>
2021-10-18 19:55:05 +0300

    Сделал лабу 11

---
+++ Lab11/Lab11_1/Source1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+bool zadanie1(int mas[], int masL, int num) {
+	for (int i = 0; i < masL; i += 2) {
+		if (mas[i] % num != 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int zadanie2(int mas[], int masL, int* sum, int& pro) {
+	int s = 0;
+	for (int i = 0; i < masL; i ++) {
+		if (mas[i] % 6 == 0) {
+			*sum += mas[i];
+			pro *= mas[i];
+		}
+		if (mas[i] < 0) {
+			s++;
+		}
+	}
+	return s;
+}
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int l;
+	std::cout << "Введите размер массива\n";
+	std::cin >> l;
+
+	int* mas = new int[l];
+	if (mas == NULL) {
+		std::cout << "Ошибка выделения памяти\n";
+		system("pause");
+		return 0;
+	}
+
+	std::cout << "Введите элементы массива\n";
+	for (int i = 0; i < l; i++) {
+		std::cin >> mas[i];
+	}
+
+	int n;
+	std::cout << "Введите число N\n";
+	std::cin >> n;
+	if(zadanie1(mas, l, 2)){
+		std::cout << "Все четные по индексу элементы массива кратны числу " << n << "\n";
+	}
+	else {
+		std::cout << "Не все четные по индексу элементы массива кратны числу "<< n << "\n";
+	}
+
+	int pro = 1 ,sum = 0;
+	std::cout << "В массиве " << zadanie2(mas, l, &sum, pro) << " отрицательных чисел, сумма всех элементов, значение которых кратных 6, равна " << sum << ", а произведение равно " << pro << "\n";
+	system("pause");
+	return 0;
+}
---
+++ Lab11/Lab11_2/Source2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+int factorial(int num) {
+	if (num <= 0) {
+		return 0;
+	}
+	if (num == 1) {
+		return 1;
+	}
+	return factorial(num - 1) * num;
+}
+
+double f(double x) {
+	double sum = 0;
+	for (int k = 0; k <= 6; k++) {
+		sum += pow((-3), (k + 1)) * factorial(k - 1) / pow((x + 5), (k + 1));
+	}
+	return sum;
+}
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	std::cout << "[ x ] = ...\n____________________\n";
+	for (double x = 3; x <= 7; x+=0.1) {
+		std::cout << "[ " << x << " ] = " << f(x) << "\n";
+	}
+	system("pause");
+	return 0;
+}
