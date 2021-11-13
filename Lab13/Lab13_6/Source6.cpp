69fc13a8c0ca40eed39f95e7e9dab082630edb77
VloBoo <VloBo2004@gmail/com>
2021-11-13 15:12:04 +0300

    Переделал ласт задание на работающий вариант

--- Lab13/Lab13_6/Source6.cpp
+++ Lab13/Lab13_6/Source6.cpp
@@ -9,18 +9,27 @@ int main() {
 	int n = 0;
 	std::cout << "\nВведите текст(до 100 символов):\n";
 	gets_s(s, l);
+	int size = strlen(s);
+	int new_size = size+1;
+	char* sn;
 	if (strlen(s) > 10) {
-		for (int i = 0; i < strlen(s); i++) {
-			if (s[i] <= '1' && s[i] >= '0') {
-				char* sn = new char[strlen(s) - 1];
-				strncpy(sn, s, i - 1);
-				strcat(sn, s);
-				strcpy(s, sn);
-				delete[] sn;
+		for (int i = 0; i < size; i++) {
+			if (s[i] <= '9' && s[i] >= '0') {
+				new_size--;
 			}
 		}
+		sn = new char[new_size];
+		for (int i = 0; i < size; i++) {
+			if (!(s[i] <= '9' && s[i] >= '0')) {
+				strncat(sn,s+i,1);
+			}
+		}
+		std::cout << "Строка после обработки\n\n" << sn << "\n";
 	}
-	std::cout << "Строка после обработки\n\n" << s<<"\n";
+	else {
+		std::cout << "Строка меньше 10 символов\n";
+	}
+	
 	system("pause");
 	return 0;
 }
